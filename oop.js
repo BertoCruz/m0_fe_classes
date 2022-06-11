9815e3e52925204d7b1eb8820d5f93b269fe1be5
Dani Lopez-Cruz <roberto.lc2493@gmail.com>
2022-06-11 00:19:57 -0600

    Update oop.js file.

--- oop.js
+++ oop.js
@@ -1,7 +1,28 @@
 // Write a class called Unicorn
 // it should have a dynamic name property
 // it should have a color property, that is silver by default
-// it should have a method called "say" that returns whatever string is passed in, with "*~*" at the beginning and end of the string
+// it should have a method called "say" that returns whatever string is passed
+// in, with "*~*" at the beginning and end of the string
+
+class Unicorn {
+  constructor(name) {
+    this.name = name;
+    this.color = "Silver";
+    this.message = `What's up? my name is ${this.name}`;
+  }
+
+  say(newMessage) {
+    this.message = `*~* ${newMessage} *~*`;
+    console.log(`\n${this.message}\n`);
+  }
+}
+console.log("-------------------------\n U N I C O R N")
+var unicorn1 = new Unicorn("Eustice");
+console.log(unicorn1);
+
+
+unicorn1.say("I waaaaant it thaaaaat way!");
+console.log(unicorn1);
 
 
 
@@ -10,6 +31,32 @@
 // it should have a pet property, that is a bat, by default BUT it could be dynamic if info is passed in initially
 // it should have a thirsty property, that is true by default
 // it should have a drink method. When called, the thirsty property changes to false
+
+class Vampire {
+  constructor(name) {
+    this.name = name;
+    this.pet = "bat";
+    this.thirsty = true;
+  }
+
+  drink(){
+    this.thirsty = false;
+    console.log("My thirst! it has been quenched! thank you.")
+  }
+
+  changePet(newPet) {
+    this.pet = newPet;
+    console.log(`My new pet ${this.pet}, I shall love you`);
+  }
+}
+
+ console.log("-------------------------\n V A M P I R E")
+ var vampire1 = new Vampire("Blade");
+ console.log(vampire1);
+
+ vampire1.drink();
+ vampire1.changePet("Nomak");
+ console.log(vampire1);
 
 
 
@@ -21,6 +68,33 @@
 //  it should have a eat method. If the dragon eats 4 times, it is no longer hungry
   // HINT: This requires you to use your knowledge from the conditionals lesson
 
+class Dragon {
+  constructor (name, rider, color){
+  this.name = name;
+  this.rider = rider;
+  this.color = color;
+  this.isHungry = true;
+  this.eat = 4;
+  }
+
+
+  changeEat(newEat) {
+    this.eat = newEat;
+    if (this.eat >= 4){
+      this.isHungry = true;
+    } else {
+      this.isHungry = false;
+    }
+  }
+}
+
+
+console.log("-------------------------\n D R A G O N")
+var dragon1 = new Dragon ("Toothless", "Hiccup", "black");
+console.log(dragon1);
+
+dragon1.changeEat(2);
+console.log(dragon1);
 
 
 //  BIG CHALLNEGE: Write a Hobbit class
@@ -31,3 +105,49 @@
 //  it should have an isAdult property (boolean) that is false by default. once a Hobbit is 33, it should be an adult
 //  it should have an isOld property that defaults to false. once a Hobbit is 101, it is old.
 //  it should have a hasRing property. If the Hobbit's name is "Frodo", true, if not, false.
+
+class Hobbit {
+  constructor(name, disposition) {
+    this.name = name;
+    this.disposition = disposition;
+    this.age = 0;
+    this.isAdult = false;
+    //once a Hobbit is 33, it should be an adult
+    this.isOld = false;
+    //once a Hobbit is 101, it is old.
+    this.hasRing = false;
+    //If the Hobbit's name is "Frodo", true, if not, false.
+
+    if(this.name === "Frodo"){ this.hasRing = true;}
+  }
+
+  celebrateBirthday() {
+    for (var i = 0; i <= 101; i++){
+      this.age = i;
+      console.log(this.age);
+
+      if(this.age === 33) {
+        this.isAdult = true;
+        console.log(`You are now... an ADULT, my dear ${this.name}! ;)`);
+      } else if(this.age === 101) {
+        this.isOld = true;
+        console.log(`My My My how you've grown up so fast, wise ${this.name}! ;)`);
+      }
+    }
+  }
+}
+
+console.log("-------------------------\n H O B B I T")
+
+var hobbit1 = new Hobbit("Frodo",["Brave, Selfless, Thoughtful, Wise, Observant"]);
+var hobbit2 = new Hobbit("Sam", ["Loyal, Supportive, Defender, Educate, Jolly"]);
+
+console.log(hobbit1);
+console.log(hobbit2);
+
+console.log("--------Hobbit1--------")
+hobbit1.celebrateBirthday();
+console.log(hobbit1);
+console.log("--------Hobbit2--------")
+hobbit2.celebrateBirthday();
+console.log(hobbit2);
